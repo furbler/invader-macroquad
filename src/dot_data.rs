2e97e540b7ab460b2f1dddfe537b1c116509244a
furbler <63544661+furbler@users.noreply.github.com>
2022-11-13 14:57:01 +0900

    描画時の色とサイズを変更

--- src/dot_data.rs
+++ src/dot_data.rs
@@ -1,30 +1,48 @@
 use std::io::Write;
-pub struct DotData {
-    pub width: u16,       // 幅[ドット]
-    pub height: u16,      // 高さ[ドット]
-    pub dot_map: Vec<u8>, // 描画部分は#、非描画部分は_で表す
+pub struct DotShape {
+    pub width: u16,              // 幅[ドット]
+    pub height: u16,             // 高さ[ドット]
+    pub dot_map: Vec<Vec<bool>>, // 描画部分は#、非描画部分は_で表す
 }
 
-fn convert_dot_map(dot_map: Vec<&str>) -> Vec<u8> {
-    let red: Vec<u8> = vec![255, 0, 0, 255];
-    let background: Vec<u8> = vec![0, 0, 0, 0];
-
-    let mut bytes: Vec<u8> = Vec::new();
-    for line in dot_map {
-        let space_removed = line.replace(' ', "");
-        for c in space_removed.chars() {
-            if c == '#' {
-                bytes.write(&red).unwrap();
-            } else if c == '_' {
-                bytes.write(&background).unwrap();
+impl DotShape {
+    // 真偽値で表されたドットマップをmacroquadで利用できるVec<u8>に変換
+    pub fn create_color_dot_map(&self, color_str: &str) -> Vec<u8> {
+        // 指定されたサイズと実際のドットマップのサイズが一致しているか確認
+        if self.height as usize != self.dot_map.len() {
+            panic!("指定されたドットマップの高さが実際のデータと異なります。");
+        }
+        if self.width as usize != self.dot_map[0].len() {
+            panic!("指定されたドットマップの幅が実際のデータと異なります。");
+        }
+        // ドットマップの幅が異なる行が無いか確認
+        let map_width = self.dot_map[0].len();
+        for l in &self.dot_map {
+            if l.len() != map_width {
+                panic!("ドットマップの形が不正です。");
             }
         }
+
+        let color = set_color(color_str);
+        let background: Vec<u8> = vec![0, 0, 0, 0];
+
+        let mut bytes: Vec<u8> = Vec::new();
+        for line in &self.dot_map {
+            for c in line {
+                if *c {
+                    bytes.write(&color).unwrap();
+                } else {
+                    bytes.write(&background).unwrap();
+                }
+            }
+        }
+        bytes
     }
-    bytes
 }
 
-pub fn dot_data(name: &str) -> DotData {
-    let player = DotData {
+// ドットデータを変更する際はこの中身のみ変更する
+pub fn dot_data(name: &str) -> DotShape {
+    let player = DotShape {
         width: 13,
         height: 8,
         dot_map: convert_dot_map(vec![
@@ -38,7 +56,7 @@ pub fn dot_data(name: &str) -> DotData {
             "# # # # # # # # # # # # #",
         ]),
     };
-    let crab_down = DotData {
+    let crab_down = DotShape {
         width: 11,
         height: 8,
         dot_map: convert_dot_map(vec![
@@ -52,7 +70,7 @@ pub fn dot_data(name: &str) -> DotData {
             "_ _ _ # # _ # # _ _ _",
         ]),
     };
-    let crab_banzai = DotData {
+    let crab_banzai = DotShape {
         width: 11,
         height: 8,
         dot_map: convert_dot_map(vec![
@@ -75,5 +93,32 @@ pub fn dot_data(name: &str) -> DotData {
             "{}のドットマップ取得に失敗しました。プログラムを終了します。",
             name
         ), // ドットマップ取得失敗
+    }
+}
+// 描画部分を真、非描画部分を偽とするドットマップを返す
+fn convert_dot_map(dot_map: Vec<&str>) -> Vec<Vec<bool>> {
+    let mut bool_map = Vec::new();
+    for line in dot_map {
+        let mut bool_line = Vec::new();
+        // 空白をすべて削除
+        let space_removed = line.replace(' ', "");
+        for c in space_removed.chars() {
+            if c == '#' {
+                bool_line.push(true);
+            } else if c == '_' {
+                bool_line.push(false);
+            }
+        }
+        bool_map.push(bool_line);
+    }
+    bool_map
+}
+
+// 指定した色に対応するrgbaの値を返す
+fn set_color(color: &str) -> Vec<u8> {
+    match color {
+        "RED" => vec![255, 0, 0, 255],
+        "TURQUOISE" => vec![68, 200, 210, 255], // 青緑色
+        _ => panic!("{}色には対応していません。プログラムを終了します。", color),
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use dot_data::DotData;
+use dot_data::DotShape;
 use macroquad::prelude::*;
 use std::error::Error;
 
@@ -6,31 +6,36 @@ mod dot_data;
 use crate::dot_data::dot_data;
 
 struct Character {
-    width: f32,
-    height: f32,
+    width: f32,  // 描画サイズの幅
+    height: f32, // 描画サイズの高さ
     texture: Texture2D,
 }
 #[macroquad::main("invader-macroquad")]
 async fn main() -> Result<(), Box<dyn Error>> {
+    // キャラクターのドット絵
+    let player_data = dot_data("player");
+    let crab_down_data = dot_data("crab_down");
+    let crab_banzai_data = dot_data("crab_banzai");
+    // ここで実際の描画サイズと色を指定する
     let player = Character {
-        width: 50.,
-        height: 50.,
-        texture: dot_map2texture(dot_data("player")),
+        width: player_data.width as f32 * 3.,
+        height: player_data.height as f32 * 3.,
+        texture: dot_map2texture("TURQUOISE", player_data),
     };
     let crab_down = Character {
-        width: 50.,
-        height: 50.,
-        texture: dot_map2texture(dot_data("crab_down")),
+        width: crab_down_data.width as f32 * 3.,
+        height: crab_down_data.height as f32 * 3.,
+        texture: dot_map2texture("RED", crab_down_data),
     };
     let crab_banzai = Character {
-        width: 50.,
-        height: 50.,
-        texture: dot_map2texture(dot_data("crab_banzai")),
+        width: crab_banzai_data.width as f32 * 3.,
+        height: crab_banzai_data.height as f32 * 3.,
+        texture: dot_map2texture("RED", crab_banzai_data),
     };
 
     loop {
         // 背景色描画
-        clear_background(LIGHTGRAY);
+        clear_background(BLACK);
         // 描画
         draw_texture_ex(
             player.texture,
@@ -69,8 +74,12 @@ async fn main() -> Result<(), Box<dyn Error>> {
 }
 
 // ドットデータをテクスチャデータに変換
-fn dot_map2texture(chara: DotData) -> Texture2D {
-    let texture = Texture2D::from_rgba8(chara.width, chara.height, &chara.dot_map);
+fn dot_map2texture(color: &str, chara: DotShape) -> Texture2D {
+    let texture = Texture2D::from_rgba8(
+        chara.width,
+        chara.height,
+        &chara.create_color_dot_map(color),
+    );
     texture.set_filter(FilterMode::Nearest);
     texture
 }
