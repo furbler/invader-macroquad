c017821d0b8d6f64fac3af8c1cb59abe6ce1cbfe
furbler <63544661+furbler@users.noreply.github.com>
2022-11-14 19:38:21 +0900

    敵のインベーダーを動かす

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,68 @@ use std::error::Error;
 mod dot_data;
 use crate::dot_data::dot_data;
 
-struct Character {
+struct Player {
     width: f32,  // 描画サイズの幅 [pixel]
     height: f32, // 描画サイズの高さ [pixel]
-    pos: Vec2,
+    pos: Vec2,   // 中心位置
     texture: Texture2D,
 }
+impl Player {
+    // プレイヤー移動
+    fn update(&mut self) {
+        if 0. < self.pos.x - self.width / 2.
+            && (is_key_down(KeyCode::A) || is_key_down(KeyCode::Left))
+        {
+            self.pos.x -= 5.;
+        }
 
-impl Character {
+        if self.pos.x + self.width / 2. < screen_width()
+            && (is_key_down(KeyCode::D) || is_key_down(KeyCode::Right))
+        {
+            self.pos.x += 5.;
+        }
+    }
     fn draw(&self) {
         draw_texture_ex(
             self.texture,
+            self.pos.x - self.width / 2.,
+            self.pos.y - self.height / 2.,
+            WHITE,
+            DrawTextureParams {
+                dest_size: Some(Vec2::new(self.width, self.height)),
+                ..Default::default()
+            },
+        )
+    }
+}
+
+struct Enemy {
+    width: f32,                // 描画サイズの幅 [pixel]
+    height: f32,               // 描画サイズの高さ [pixel]
+    pos: Vec2,                 // 中心位置
+    switched_time: f64,        // 最後に画像切り替えした時間
+    select_texture: bool,      // どちらの状態の画像を表示するか
+    first_texture: Texture2D,  // 状態1
+    second_texture: Texture2D, // 状態2
+}
+impl Enemy {
+    fn draw(&mut self) {
+        let current_time = get_time();
+        // 表示画像切り替えから一定時間経過していたら
+        if current_time - self.switched_time > 1. {
+            // 表示画像切り替え
+            self.select_texture = !self.select_texture;
+            self.switched_time = current_time;
+        }
+        let texture;
+        if self.select_texture {
+            texture = self.first_texture;
+        } else {
+            texture = self.second_texture;
+        }
+
+        draw_texture_ex(
+            texture,
             self.pos.x - self.width / 2.,
             self.pos.y - self.height / 2.,
             WHITE,
@@ -34,37 +85,24 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let crab_down_data = dot_data("crab_down");
     let crab_banzai_data = dot_data("crab_banzai");
     // ここで実際の描画サイズと色を指定する
-    let mut player = Character {
+    let mut player = Player {
         width: player_data.width as f32 * 3.,
         height: player_data.height as f32 * 3.,
         pos: Vec2::new(screen_width() / 2., screen_height() - 120.),
         texture: dot_map2texture("TURQUOISE", player_data),
     };
-    let mut crab_down = Character {
+    let mut crab = Enemy {
         width: crab_down_data.width as f32 * 3.,
         height: crab_down_data.height as f32 * 3.,
         pos: Vec2::new(screen_width() / 3., screen_height() / 3.),
-        texture: dot_map2texture("PURPLE", crab_down_data),
-    };
-    let mut crab_banzai = Character {
-        width: crab_banzai_data.width as f32 * 3.,
-        height: crab_banzai_data.height as f32 * 3.,
-        pos: Vec2::new(screen_width() * 0.7, screen_height() / 3.),
-        texture: dot_map2texture("PURPLE", crab_banzai_data),
+        switched_time: get_time(),
+        select_texture: true,
+        first_texture: dot_map2texture("PURPLE", crab_banzai_data),
+        second_texture: dot_map2texture("PURPLE", crab_down_data),
     };
 
     loop {
-        if 0. < player.pos.x - player.width / 2.
-            && (is_key_down(KeyCode::A) || is_key_down(KeyCode::Left))
-        {
-            player.pos.x -= 5.;
-        }
-
-        if player.pos.x + player.width / 2. < screen_width()
-            && (is_key_down(KeyCode::D) || is_key_down(KeyCode::Right))
-        {
-            player.pos.x += 5.;
-        }
+        player.update();
         // 背景色描画
         clear_background(BLACK);
         // プレイヤーの下の横線
@@ -76,11 +114,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
             3.,
             RED,
         );
-
         // 描画
         player.draw();
-        crab_down.draw();
-        crab_banzai.draw();
+        crab.draw();
 
         next_frame().await
     }
